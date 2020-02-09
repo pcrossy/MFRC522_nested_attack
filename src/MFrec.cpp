c81ad89089b358941be254cddd08fc193dff3fc9
pcrossy <twelli@yahoo.de>
2020-02-10 00:44:16 +0100

    creating horrible c code

--- src/MFrec.cpp
+++ src/MFrec.cpp
@@ -107,8 +107,9 @@ bool MFrec::automatedCrackKey( byte command, byte blockAddr_e, byte blockAddr_a,
     for( int ikey = 0; ikey < nDefaultKeys; ikey++ )
     {
         resetPICC( delayTime );
-        byte *fullkey[6] = defaultKeys[ikey];
-        printf("Trying default key: %x\n", fullkey);
+        byte *fullkey[6] = {0,0,0,0,0,0};
+        fullkey = defaultKeys[ikey];
+        printf("Trying default key: %x\n", *fullkey);
         /*-------------------------------------- get nonce distance  ---------------------------------------*/
 	    if( !authenticateManually( command, blockAddr_e, &n_T, defaultKeys[ikey] ) )  // ( byte command, byte blockAddr, uint32_t *n_T, byte *key /*=nullptr*/ )
 	       {
